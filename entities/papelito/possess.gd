26d6cc1952836235d4808942115bc2141942e546
diejor <diejorarr@gmail.com>
2024-10-02 11:10:49 -0500

    working commit

--- entities/papelito/possess.gd
+++ entities/papelito/possess.gd
@@ -6,19 +6,20 @@ class_name Possess
 @onready var state_manager: StateManager = %StateManager
 @onready var state_chart: StateChart = %StateChart
 
-var can_shift = false
+var on_floor = true
 
 func _ready():
 	un_possess()
-	state_chart.set_expression_property("can_shift", can_shift)
+	state_chart.set_expression_property("on_floor", on_floor)
 
 func _process(_delta: float) -> void:
 	%PapelitoCam.global_rotation = 0
 
 func _physics_process(delta: float) -> void:
+	on_floor = is_on_floor()
+	state_chart.set_expression_property("on_floor", on_floor)
 
 func possess():
-	print("possess first")
 	cast_to_floor()
 	visible = true
 	process_mode = PROCESS_MODE_INHERIT
@@ -26,16 +27,11 @@ func possess():
 	%PapelitoCam.transform = Transform2D.IDENTITY
 
 func un_possess():
-	print("un_possess first")
 	if self.transform != Transform2D.IDENTITY:
 		get_parent().transform = self.global_transform
 	self.transform = Transform2D.IDENTITY
 	visible = false
 	process_mode = PROCESS_MODE_DISABLED
-	can_shift = check_can_shift()
-
-func check_can_shift() -> bool:
-	return 
 
 func did_input(action: StringName) -> bool:
 	return state_manager.input_buffer.any(func(entry): return entry.input.is_action(action));
